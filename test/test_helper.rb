c2fe3dbe955e3b388b6bead4b5994d7adc05f203
Andreas Böhrnsen <andreas@frontrunner.io>
2019-05-19 18:22:52 +0000

    enable minitest again

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-#require "minitest/rails"
+require "minitest/rails"
 
 # Uncomment for awesome colorful output
 # require "minitest/pride"
