a96a2eae39e8483402e3c7c0bd7b9f5dbac84a91
Andreas Böhrnsen <andreas@frontrunner.io>
2019-05-19 18:19:20 +0000

    fix test

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -2,8 +2,9 @@ require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
   test "visiting the index" do
-    visit posts_url
+    visit posts_path
 
     assert_selector "h1", text: "Posts"
+    assert_selector "table"
   end
 end
